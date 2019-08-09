cce7838362e7d2fccffd9fa50130d1a87f9eaaa4
James Steel <hello@hashandsalt.com>
2019-08-09 08:54:21 +0100

    Better Route

--- index.php
+++ index.php
@@ -3,22 +3,17 @@
 require_once __DIR__ . '/functions/index.php';
 
 Kirby::plugin('hashandsalt/abt', [
-
     'routes' => [
-      [
-          'pattern' => '(:any)',
-          'action'  => function ($id) {
-          if (visitorgroup('a') === true) {
-              return page($id);
-            } else {
-              if ($page = page($id)->find('b')) {
-                  return page($page->id());
-              } else {
-                  return page($id);
+        [
+            'pattern' => '(:any)',
+            'action' => function ($id) {
+                if (visitorgroup('a') !== true) {
+                    if ($pageB = page($id)->find('b')) {
+                        return $pageB;
+                    }
                 }
+                $this->next();
             }
-          }
         ]
     ],
-
 ]);
